b61dcab43e02e3d00881de6909b0af0e0231918e
Ukasyah <hi.ukasyah@gmail.com>
2024-07-27 15:10:12 +0700

    update spinner

---
+++ src/lib/components/feedback/Spinner.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { HTMLHtmlAttributes } from 'svelte/elements';
+
+	type $$Props = HTMLHtmlAttributes & {
+		strokeWidth?: number;
+	};
+
+	export let strokeWidth: $$Props['strokeWidth'] = 2.75;
+</script>
+
+<svg
+	width="24"
+	height="24"
+	stroke="inherit"
+	viewBox="0 0 24 24"
+	xmlns="http://www.w3.org/2000/svg"
+	{...$$restProps}
+>
+	<g class="spinner_V8m1">
+		<circle cx="12" cy="12" r="9.5" fill="none" stroke-width={strokeWidth}></circle>
+	</g>
+</svg>
